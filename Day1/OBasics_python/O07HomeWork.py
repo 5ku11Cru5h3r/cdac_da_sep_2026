3c0387f85b74b15ec96f679c656ddcbd7bce9320
5ku11cru5h3r <th3.yash.jha@proton.me>
2026-09-24 17:29:32 +0530

    First Commit

--- Day1/OBasics_python/O07HomeWork.py
+++ Day1/OBasics_python/O07HomeWork.py
@@ -2,25 +2,32 @@
 Develop a context manager
 1. context manager work with "with" statements
 '''
+import sqlite3
+
 
 class CA:
-    def __init__(self):
-        print("self",id(self))
-       
+    def __init__(self, db):
+        self.db = db
+        self.connection = None
+        # self.cursor = None
+        # print("self", id(self))
+
     def fun(self):
         print("fun")
 
     def __enter__(self):
-        print("Enter")
-
+        print("Entered the function")
+        self.connection = sqlite3.connect(self.db)
+        self.cursor = self.connection.cursor()
+        return self.cursor()
     def __exit__(self, exc_type, exc, tb):
         print("Exit")
 
 # obj = CA()
 # print(id(obj))
 
+
 obj1 = CA()
 with obj1:
     obj1.fun()
     # print(id(obj1))
-    
